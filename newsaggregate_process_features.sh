3429ab2c29de9f7824b383027291608b0eae1926
Felix <43045151+fyx99@users.noreply.github.com>
2022-05-03 21:35:10 +0200

    Update newsaggregate_process_features.sh

--- newsaggregate_process_features.sh
+++ newsaggregate_process_features.sh
@@ -4,7 +4,7 @@
 
 docker pull fxxy/news-aggregate-ml
 
-CONTAINERID=$(docker run -d -m 700m --memory-swap="2000m" -e TASK='RSS' -e DB_NAME='newsaggregate' -e DB_HOST='172.17.0.2' -e DB_USER='postgres' -e DB_PW='u3fph3ßü98fg43f34f3' -e DB_PORT='5432' fxxy/news-aggregate-ml)
+CONTAINERID=$(docker run -d -m 700m --memory-swap="2000m" -e TASK='RSS' --env-file /env/drop.env fxxy/news-aggregate-ml)
 echo $CONTAINERID
 docker wait $CONTAINERID
 
