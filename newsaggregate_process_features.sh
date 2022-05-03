c759f601016e4f25facde10eca3b27fb902b06a5
Felix <43045151+fyx99@users.noreply.github.com>
2022-05-03 16:19:51 +0200

    Create newsaggregate_process_features.sh

---
+++ newsaggregate_process_features.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+
+
+docker pull fxxy/news-aggregate-ml
+
+CONTAINERID=$(docker run -d -m 700m --memory-swap="2000m" -e TASK='RSS' -e DB_NAME='newsaggregate' -e DB_HOST='172.17.0.2' -e DB_USER='postgres' -e DB_PW='u3fph3ßü98fg43f34f3' -e DB_PORT='5432' fxxy/news-aggregate-ml)
+echo $CONTAINERID
+docker wait $CONTAINERID
+
+START=$(docker inspect --format='{{.State.StartedAt}}' ${CONTAINERID})
+STOP=$(docker inspect --format='{{.State.FinishedAt}}' ${CONTAINERID})
+STATUS=$(docker container inspect -f '{{.State.ExitCode}}' ${CONTAINERID})
+START_TIMESTAMP=$(date --date=$START +%s)
+STOP_TIMESTAMP=$(date --date=$STOP +%s)
+MINUTES=$(( ($STOP_TIMESTAMP - $START_TIMESTAMP) / 60 ))
+
+docker exec postgres psql -U postgres -d newsaggregate -c "INSERT INTO Runs (task, containerid, status, duration) values ('FEATURE', '${CONTAINERID}', '${STATUS}', '${MINUTES}');"
