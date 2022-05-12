3dcba6ab2e624b4db5642eaa6c01947659728b96
Felix <43045151+fyx99@users.noreply.github.com>
2022-05-12 21:32:04 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-docker exec -t postgres pg_dumpall -c -U postgres | aws s3 cp - s3://hcp-1a9ec634-dfdf-40cd-8a08-d40904f2d00e/pgdumps/dump_`date +%d-%m-%Y`.sql > crontest.txt && docker exec postgres psql -U postgres -d newsaggregate -c "INSERT INTO Backups (file_name) values ('dump_`date +%d-%m-%Y`.sql');"
+echo 'In';
+docker exec -t postgres pg_dumpall -c -U postgres | aws s3 cp - s3://hcp-1a9ec634-dfdf-40cd-8a08-d40904f2d00e/pgdumps/dump_`date +%d-%m-%Y`.sql 
+echo 'Dump Done';
+docker exec postgres psql -U postgres -d newsaggregate -c "INSERT INTO Backups (file_name) values ('dump_`date +%d-%m-%Y`.sql');"
+echo 'Out';
