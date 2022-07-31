102b5e8d5531820a6f8bd8cdc9dd6ef86c91017a
Felix <43045151+fyx99@users.noreply.github.com>
2022-07-31 13:49:09 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ docker volume rm $(docker volume ls -qf dangling=true)
 rm -rf /var/lib/snapd/cache/*
 journalctl --vacuum-size=100M
 find /var/log -type f -name "*.gz" -delete
-docker pull fxxy/news-rss
-docker pull fxxy/news-feed
-docker pull fxxy/news-reprtxt
+docker pull fxxy/news-aggregate-rss
+docker pull fxxy/news-aggregate-feed
+docker pull fxxy/news-aggregate-reprtxt
 docker pull fxxy/news-aggregate-srv
