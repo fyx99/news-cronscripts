fcfea7aab712168a7ea6c665c3ac641335f5aae0
Felix <43045151+fyx99@users.noreply.github.com>
2024-12-23 20:03:59 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -t postgres pg_dumpall -c -U postgres | /usr/local/bin/aws s3 cp - s3://hcp-1a9ec634-dfdf-40cd-8a08-d40904f2d00e/pgdumps/dump_`date +%d-%m-%Y`.sql && docker exec postgres psql -U postgres -d newsaggregate -c "INSERT INTO Backups (file_name) values ('dump_`date +%d-%m-%Y`.sql');"
+docker exec -t postgres pg_dumpall -c -U postgres | /usr/local/bin/aws s3 cp - s3://111-1a9ec634-dfdf-40cd-8a08-d40904f2d00e/pgdumps/dump_`date +%d-%m-%Y`.sql && docker exec postgres psql -U postgres -d newsaggregate -c "INSERT INTO Backups (file_name) values ('dump_`date +%d-%m-%Y`.sql');"
